875d24607eaa0093fde9630983226422244a1645
Ayman Benaissa <ayman.benaissa2000@gmail.com>
2021-03-25 19:13:23 +0100

    minor cleanup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import maths
+import sys
 
 general_filter = r"[+-]? ?\d+.?\d*? \* [Xx]\^\d"
 power_filter   = r"[Xx]\^(?P<power>\d)"
@@ -8,16 +10,6 @@ equation_factors = []
 def first_degree(a, b):
     print("the solution is:" + f'{-b/a :.6f}')
 
-def sqrt(n, l) :  
-    x = n  
-    count = 0 
-    while (1) : 
-        count += 1 
-        root = 0.5 * (x + (n / x))  
-        if (abs(root - x) < l) : 
-            break   
-        x = root 
-    return root  
 
 def second_degree(a, b, c):
     delta = (b * b) - (4 * a * c)
@@ -34,8 +26,6 @@ def second_degree(a, b, c):
     print(first_solution)
     print(second_solution)
 
-def abs(x):
-    return x if x >= 0 else -x
 
 def fill_list_values(sequence, side):
     factor = 1 if side == "right" else -1
@@ -53,26 +43,25 @@ def equation_cleanup(equation_factors):
             break
         else:
             del equation_factors[i]
-    
-        
+
 def print_equation():
     reduced_equation = ""
     polynomial_degree = len(equation_factors)  - 1
     for index, x in enumerate(equation_factors):
-        reduced_equation += ("- " if x < 0 else "" if not reduced_equation else "+ ") + str(abs(x)) + " X^" + str(index) + " "
+        reduced_equation += ("- " if x < 0 else "" if not reduced_equation else "+ ") + str(abs(x)) + " * X^" + str(index) + " "
     reduced_equation += "= 0"
     print("Reduced form:", reduced_equation)
     equation_cleanup(equation_factors)
     polynomial_degree = len(equation_factors)  - 1
     print("Polynomial degree: ", polynomial_degree)
-    if polynomial_degree == 2 and equation_factors[2]:
+    if polynomial_degree == 1:
+        first_degree(equation_factors[1], equation_factors[0])
+    elif polynomial_degree == 2:
         second_degree(equation_factors[2], equation_factors[1], equation_factors[0])
-    elif polynomial_degree == 1 and equation_factors[1]:
-        first_degree(equation_factors[1], equation_factors[0])  
+
     
 
-def equation_recognition():
-    sides = input()
+def equation_recognition(sides):
     sides = sides.split("=")
     if len(sides) != 2:
         exit();
@@ -84,8 +73,6 @@ def equation_recognition():
     if equation_factors:
         print_equation()
 
-def main():
-    equation_recognition()
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        equation_recognition(sys.argv[1])
