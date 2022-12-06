23b8632ce2ccbf5296db3ee70d61b970f506321f
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-12-06 02:49:40 +0100

    Improve original solution using OOP

--- solver/solver.py
+++ solver/solver.py
@@ -1,37 +1,60 @@
+class Solver():
 
-def firstDegree(a, b):
-    print("the solution is:")
-    print(-b/a)
+    @staticmethod
+    def throwSolverWarning(warningMessage):
+        print(f'\x1b[1;37;41m Solver Warning \x1b[0m : {warningMessage}')
 
-def secondDegree(a, b, c):
-    delta = (b * b) - (4 * a * c)
-    if delta == 0:
-        print("Discriminant is null the solution is:")
-        print((-b) / (2 * a))
-    else : 
-        if delta > 0:
-            print("Discriminant is strictly positive, the two solutions are:")
-            delta = sqrt(delta)
-            first_solution = f'{(-b - delta) / (2 * a) :.6f}'
-            second_solution = f'{(-b + delta) / (2 * a) :.6f}'
-        else :
-            print("Discriminant is strictly negative, the two solutions are:")
-            delta = sqrt(abs(delta))
-            first_solution = f'{-b / 2 * a :.6f}' + " - " + f'{delta / 2 * a:.6f}' + "i"
-            second_solution = f'{-b / 2 * a :.6f}' + " + " + f'{delta / 2 * a:.6f}' + "i"
-        print(first_solution)
-        print(second_solution)
+    @staticmethod
+    def solve(equationDict):
+        endPower = max(equationDict.keys())
+        allPolinomialFactors = {k: equationDict[k] if k in equationDict.keys() else 0 for k in range(endPower + 1)} 
+        if endPower == 0:
+            if allPolinomialFactors[0] == 0:
+                print("Every real number is a solution !")
+            else:
+                Solver.throwSolverWarning("There is no solution !")
+        elif endPower == 1:
+            Solver.firstDegree(allPolinomialFactors[1], allPolinomialFactors[0])
+        elif endPower == 2:
+            Solver.secondDegree(allPolinomialFactors[2], allPolinomialFactors[1], allPolinomialFactors[0])
+        else:
+            Solver.throwSolverWarning("The polynomial degree is stricly greater than 2, I can't solve.")
 
-def sqrt(n, l = 0.000001) :  
-    x = n  
-    count = 0 
-    while (1) : 
-        count += 1 
-        root = 0.5 * (x + (n / x))  
-        if (abs(root - x) < l) : 
-            break   
-        x = root 
-    return root 
+    @staticmethod
+    def firstDegree(a, b):
+        print(f"the solution is: {(-b / a):.6f}")
 
-def abs(x):
-    return x if x >= 0 else -x
+    @staticmethod
+    def secondDegree(a, b, c):
+        delta = (b * b) - (4 * a * c)
+        if delta == 0:
+            print(f"Discriminant is null the solution is: {(-b / (2 * a)):.6f}")
+        else: 
+            if delta > 0:
+                print("Discriminant is strictly positive, the two solutions are:")
+                deltaSqrt = Solver.sqrt(delta)
+                first_solution = f'{(-b - deltaSqrt) / (2 * a) :.6f}'
+                second_solution = f'{(-b + deltaSqrt) / (2 * a) :.6f}'
+            else:
+                print("Discriminant is strictly negative, the two solutions are:")
+                deltaSqrt = Solver.sqrt(Solver.abs(delta))
+                first_solution = f'{-b / 2 * a :.6f}' + " - " + f'{deltaSqrt / 2 * a:.6f}' + "i"
+                second_solution = f'{-b / 2 * a :.6f}' + " + " + f'{deltaSqrt / 2 * a:.6f}' + "i"
+            print(first_solution)
+            print(second_solution)
+
+    @staticmethod
+    def sqrt(n, l = 0.000001):  
+        x = n  
+        count = 0 
+        while (1) : 
+            count += 1 
+            root = 0.5 * (x + (n / x))  
+            if (abs(root - x) < l) : 
+                break   
+            x = root 
+        return root 
+    
+    @staticmethod
+    def abs(x):
+        return x if x >= 0 else -x
