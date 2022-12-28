a5d578e0f0630cb640586282203c74087dac90ce
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-12-28 18:10:23 +0100

    Rename parser file

---
+++ solver/computor.py
@@ -0,0 +1,12 @@
+from equation_parser import Parser
+from solver import Solver
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py \"equation\"")
+        exit()
+    parser = Parser(sys.argv[1])
+    parser.parseEquation()
+    Solver.solve(parser.equationFactors)
+    
